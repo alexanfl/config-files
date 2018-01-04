468403efed57343cf1c0854d576faed0a3a0571f
Alexander Fleischer <fleisch91@gmail.com>
2018-01-04 13:54:24 +0100

    Ruined an if-statement when updating xal email script

--- i3stuff/arch-laptop-2016-07/.config/i3blocks/email-xal.py
+++ i3stuff/arch-laptop-2016-07/.config/i3blocks/email-xal.py
@@ -23,4 +23,5 @@ unread_msg_nums = response[0].split()
 emails = len(unread_msg_nums)
 if emails == 0:
     print('<span color="#7898cc" font="FontAwesome">\uf0e0</span> <span color="#ffffff"><sup>%d</sup></span>' % emails)
+else:
     print('<span color="#7898cc" font="FontAwesome">\uf0e0</span> <span color="#00ff00"><sup>%d</sup></span>' % emails)
