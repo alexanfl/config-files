f9147cc4f1f4e40047e46bf4e24754994b20c817
Alexander Fleischer <alexander.fleischer@statkraft.com>
2022-12-19 16:16:25 +0100

    Change icon

--- i3stuff/.config/i3blocks/ethernet.py
+++ i3stuff/.config/i3blocks/ethernet.py
@@ -9,8 +9,8 @@ cmd = f"cat /sys/class/net/{interface}/operstate".split()
 output = str(subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0])
 
 if 'down' in output:
-    print('<span color="#e89393" font="FontAwesome">\uf176\uf175</span>')
+    print('<span color="#e89393" font="FontAwesome">↯</span>')
 elif 'up' in output:
-    print('<span color="#88b090" font="FontAwesome">\uf176\uf175</span>')
+    print('<span color="#88b090" font="FontAwesome">↯</span>')
 else:
     print('Wrong interface! Edit ethernet.py.')
