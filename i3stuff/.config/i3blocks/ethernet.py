d3b9bbc8656046ab370514b812571fca4d731db1
Alexander Fleischer <alexander.fleischer@statkraft.com>
2022-12-20 08:50:16 +0100

    Add more robust ethernet script

--- i3stuff/.config/i3blocks/ethernet.py
+++ i3stuff/.config/i3blocks/ethernet.py
@@ -2,11 +2,27 @@ import subprocess
 import sys
 
 
-interface = sys.argv[1]
+
+def get_interface():
+    cmd = ['nmcli', 'c']
+    output = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0].decode()
+    for line in output.split('\n'):
+        vals = line.split()
+        if len(vals) < 4:
+            continue
+
+        if 'ethernet' in line:
+            return vals[-1]
+
+
+interface = get_interface() if len(sys.argv) < 2 else sys.argv[1]
+
 
 cmd = f"cat /sys/class/net/{interface}/operstate".split()
 
 output = str(subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0])
+
+
 
 if 'down' in output:
     print('<span color="#e89393" font="FontAwesome">↯</span>')
