b345e68b54b5d26855f759e57ee67dd9f8f6fbaa
Alexander Fleischer <alexander.fleischer@statkraft.com>
2023-01-09 13:51:34 +0100

    Improve wifi script

--- i3stuff/.config/i3blocks/wifi.py
+++ i3stuff/.config/i3blocks/wifi.py
@@ -3,24 +3,24 @@ import sys
 import re
 
 icon = ''
-color_def="#666666"		# gray
-color_good="#88b090"	# green
-color_warn="#ccdc90"	# yellow
-color_crit="#e89393"	# red
-color_info="#fce94f"	# bright yellow
+color_def="#666666"   # gray
+color_good="#88b090"  # green
+color_warn="#ccdc90"  # yellow
+color_crit="#e89393"  # red
+color_info="#fce94f"  # bright yellow
 
-FORMAT = f'<span color="%s">{icon} %s (%s %%) [%s %s]</span>'
+FORMAT = f'<span color="%s">{icon} %s (%s %%) [%s]</span>'
 cmd = ['nmcli', 'dev', 'wifi']
 
 output = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0].decode()
 res = None
+regex_pattern = r'\s{2,}'
 for line in output.split('\n'):
     if line.startswith('*'):
-        res = line.split()
-        ssid = res[2]
-        signal = int(res[7])
-        speed = res[5]
-        unit = res[6]
+        matches = re.split(regex_pattern, line)
+        ssid = matches[2]
+        signal = int(matches[6])
+        speed = matches[5]
 
         if signal > 70:
             color = color_good
@@ -31,7 +31,7 @@ for line in output.split('\n'):
         else:
             color = color_info
             
-        fmt = FORMAT % (color, ssid, signal, speed, unit)
+        fmt = FORMAT % (color, ssid, signal, speed)
         print(fmt)
 
 
