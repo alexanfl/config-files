fc52a860245f5df423ddfb2bc8e83ae4bcc9437d
Alexander Fleischer <alexander.fleischer@statkraft.com>
2025-11-12 12:43:52 +0100

    Improve battery alert script

--- swaystuff/scripts/battery-alert.sh
+++ swaystuff/scripts/battery-alert.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-BATTERY=$(upower -i $(upower -e | grep BAT) | grep percentage | awk '{print $2}' | sed 's/%//')
-if [ "$BATTERY" -le 50 ]; then
-    notify-send -u low "Half battery left" "Battery at ${BATTERY}%"
-fi
-if [ "$BATTERY" -le 30 ]; then
-    notify-send -u normal "Battery Low" "Battery at ${BATTERY}%"
-fi
-if [ "$BATTERY" -le 20 ]; then
-    notify-send -u critical "Battery very Low" "Battery at ${BATTERY}%"
-fi
-if [ "$BATTERY" -le 10 ]; then
-    notify-send -u critical "Battery Critical" "Plug in your charger!"
-fi
+LAST_ALERT=100  # start with a high value
+BATTERY_PATH=$(upower -e | grep BAT | head -n 1)
 
+while true; do
+    BATTERY=$(upower -i "$BATTERY_PATH" | awk '/percentage/ {gsub("%","",$2); print $2}')
+
+    if [ "$BATTERY" -le 10 ] && [ "$LAST_ALERT" -gt 10 ]; then
+        notify-send -u critical "Battery Critical" "Plug in your charger!"
+        LAST_ALERT=10
+    elif [ "$BATTERY" -le 20 ] && [ "$LAST_ALERT" -gt 20 ]; then
+        notify-send -u critical "Battery Very Low" "Battery at ${BATTERY}%"
+        LAST_ALERT=20
+    elif [ "$BATTERY" -le 30 ] && [ "$LAST_ALERT" -gt 30 ]; then
+        notify-send -u normal "Battery Low" "Battery at ${BATTERY}%"
+        LAST_ALERT=30
+    elif [ "$BATTERY" -le 50 ] && [ "$LAST_ALERT" -gt 50 ]; then
+        notify-send -u low "Half Battery Left" "Battery at ${BATTERY}%"
+        LAST_ALERT=50
+    fi
+
+    sleep 300
+done
